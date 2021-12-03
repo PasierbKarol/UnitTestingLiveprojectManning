03f8bef12e330dfd9e4cd0c46f7ef9310ee0106e
Karol Pasierb <pasierbkarol@gmail.com>
2021-12-03 17:38:17 +0000

    Updated solution for 2.1

--- HotspotOptimizerLibrary/src/test/java/BugsTest.java
+++ HotspotOptimizerLibrary/src/test/java/BugsTest.java
@@ -1,5 +1,6 @@
 import com.assetco.hotspots.optimization.SearchResultHotspotOptimizer;
 import com.assetco.search.results.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,35 +12,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BugsTest {
 
-    protected SearchResults searchResults;
-    protected SearchResultHotspotOptimizer sut;
+    private SearchResults searchResults;
+    private SearchResultHotspotOptimizer optimizer;
+    private AssetVendor partnerVendor;
 
     @BeforeEach
     void setup() {
+        optimizer = new SearchResultHotspotOptimizer();
         searchResults = new SearchResults();
-        sut = new SearchResultHotspotOptimizer();
+        partnerVendor = makeVendor(AssetVendorRelationshipLevel.Partner);
     }
 
     @Test
     public void precedingPartnerWithLongTrailingAssetsDoesNotWin() {
-        AssetVendor bigShotsPartner = makeVendor(AssetVendorRelationshipLevel.Partner);
-        AssetVendor celebPartner = makeVendor(AssetVendorRelationshipLevel.Partner);
-        Asset missingBigShots = givenAssetInResultsWithVendor(bigShotsPartner);
-        Asset celeb = givenAssetInResultsWithVendor(celebPartner);
+        Asset missing = givenAssetInResultsWithVendor(partnerVendor);
+        givenAssetInResultsWithVendor(makeVendor(AssetVendorRelationshipLevel.Partner));
 
         ArrayList<Asset> expected = new ArrayList<>();
-       expected.add(celeb);
-       expected.add(givenAssetInResultsWithVendor(bigShotsPartner));
-       expected.add(givenAssetInResultsWithVendor(bigShotsPartner));
-       expected.add(givenAssetInResultsWithVendor(bigShotsPartner));
+//        expected.add(missing);
+        expected.add(givenAssetInResultsWithVendor(partnerVendor));
+        expected.add(givenAssetInResultsWithVendor(partnerVendor));
+        expected.add(givenAssetInResultsWithVendor(partnerVendor));
+        expected.add(givenAssetInResultsWithVendor(partnerVendor));
 
         whenOptimise();
-        thenHotspotDoesNotHave(HotspotKey.Showcase, missingBigShots);
+        thenHotspotDoesNotHave(HotspotKey.Showcase, missing);
         thenHotspotHasExactly(HotspotKey.Showcase, expected);
     }
 
     private AssetVendor makeVendor(AssetVendorRelationshipLevel level) {
-        return Any.vendor();
+        return Any.vendorWithLevel(level);
     }
 
     private Asset givenAssetInResultsWithVendor(AssetVendor assetVendor) {
@@ -49,17 +51,15 @@ public class BugsTest {
     }
 
     private void whenOptimise() {
-        sut.optimize(searchResults);
+        optimizer.optimize(searchResults);
     }
 
-    private void thenHotspotDoesNotHave(HotspotKey key, Asset... assets) {
-        var members = searchResults.getHotspot(key).getMembers();
-        for (Asset asset : assets) {
-            assertFalse(members.contains(asset));
-        }
+    private void thenHotspotDoesNotHave(HotspotKey key, Asset... forbidden) {
+        for (var asset : forbidden)
+            assertFalse(searchResults.getHotspot(key).getMembers().contains(asset));
     }
 
-    private void thenHotspotHasExactly(HotspotKey key, List<Asset> expected) {
-        assertArrayEquals(expected.toArray(), searchResults.getHotspot(key).getMembers().toArray());
+    private void thenHotspotHasExactly(HotspotKey hotspotKey, List<Asset> expected) {
+        Assertions.assertArrayEquals(expected.toArray(), searchResults.getHotspot(hotspotKey).getMembers().toArray());
     }
 }
